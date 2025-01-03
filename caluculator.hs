7e4718cf71bf01af99365741d0d3292bce780de5
mocoatsu <mocomocoaimai673@gmail.com>
2025-01-03 14:00:13 +0900

    feat: calculate Recovery Delay

--- caluculator.hs
+++ caluculator.hs
@@ -1,3 +1,7 @@
 caluculateQBcount :: Double -> Double -> Int
 caluculateQBcount enCapacity qbConsumptionEN =
   ceiling (enCapacity / qbConsumptionEN)
+
+caluculateRecoveryDelay :: Double -> Double
+caluculateRecoveryDelay enRecharge =
+  1000 / enRecharge
